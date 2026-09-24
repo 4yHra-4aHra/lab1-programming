3ecb370d79a0cec04fca30f65a0d1e99bebbd717
Ярославлев Игорь <igor.yaroslavlev@mail.ru>
2026-09-24 14:05:11 +0300

    Four commit: Для скриншота для лабораторки)))

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
-#include "iostream"
+#include <iostream>
 #include <string>
-
-auto square(int x) -> int{
-    return x * x;
-}
 
 int main() {
     std::cout << "Hello World!!!" << std::endl;
@@ -12,14 +8,6 @@ int main() {
     std::cout << "Студент: " << name << std::endl;
     
     std::cout << "Дата: 2026-09-10" << std::endl;
-
-    auto number{15};
-
-    auto result = square(number);
-
-    std::cout << "\n" << std::endl;
-    std::cout << "Число: " << number << std::endl;
-    std::cout << "Результат:" << result << std::endl;
 
     return 0;
 }
--- main.py
+++ main.py
@@ -2,12 +2,3 @@ print('Hello world!!!')
 name = 'Ярославлев Игорь'
 print(f'Студент: {name}')
 print('Дата: 2026-09-10')
-
-
-def square(x):
-    return x * x
-
-number = int(input('Введите число: '))
-result = square(number)
-
-print(f'Квадрат: {result}')
